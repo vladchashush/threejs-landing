003ef549bd7f923e4f738c999391975f12578293
vladchashush <vladchashush@gmail.com>
2024-12-10 15:11:09 +0400

    prepare to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const prod = process.env.NODE_ENV === 'production'
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
+const nextConfig = prod
+	? {
+			output: 'export',
+			basePath: baseUrl,
+			distDir: './docs'
+			// images: {
+			// 	loader: 'custom',
+			// 	loaderFile: './customImageLoader.ts'
+			// }
+		}
+	: {}
+
+export default nextConfig
